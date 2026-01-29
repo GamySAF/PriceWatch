d38eac0ae24f784f4bbbf9f95e291a1478f11837
GamySAF <gammiigamy@gmail.com>
2026-01-29 20:04:36 +0300

    fixing protected route

--- frontend/src/components/protectedRoute.jsx
+++ frontend/src/components/protectedRoute.jsx
@@ -1,8 +1,12 @@
 import { Navigate } from "react-router-dom";
-
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/login" />;
-};
 
+  // This ensures that "null", "undefined", or empty strings are all treated as "No Token"
+  if (!token || token === "null" || token === "undefined") {
+    return <Navigate to="/login" replace />;
+  }
+  
+  return children;
+};
 export default ProtectedRoute;
